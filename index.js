6beb9a15f323186eb2c476abe5f622a264f9873a
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2021-10-12 04:27:46 +0530

    nodejs_auth

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+
 // Connect to DB
 const mongoose = require("mongoose");
 // Import Routes
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 
-const dotenv = require("dotenv");
 dotenv.config();
 
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
@@ -15,6 +17,7 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
 
 //Middlewares
 app.use(express.json());
+app.use(cors());
 // Route Middlewares
 app.use("/api/user", authRoute);
 app.use("/api/posts", postRoute);
