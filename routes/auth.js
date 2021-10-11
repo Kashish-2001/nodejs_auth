ca0593c5aa361d5515bbbbd2c529c36fd6cbb18d
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2021-10-12 03:08:22 +0530

    added login api with validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,48 @@
 const router = require("express").Router();
 const User = require("../model/User");
-const { registerValidation } = require("../validation");
+const { registerValidation, loginValidation } = require("../validation");
+const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
-  //LETS VALIDATE THE DATA BEFORE WE MAKE A USER
+  // LETS VALIDATE THE DATA BEFORE WE MAKE A USER
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   // Checking if user already exists
-  const emailExist = User.findOne({ email: req.body.email });
+  const emailExist = await User.findOne({ email: req.body.email });
   if (emailExist) return res.status(400).send("Email already exist");
+
+  // Hash passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
   // Create a new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
+    res.send({ user: user._id }); // what is the difference between id and _id, both gives same response
   } catch (err) {
     res.status(400).send(err);
   }
+});
+
+router.post("/login", async (req, res) => {
+  // LETS VALIDATE THE DATA BEFORE WE LOG IN THE USER
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Email doesn't exist
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email or password is wrong");
+
+  // Wrong password
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Invalid password");
+  res.send("Logged in");
 });
 
 module.exports = router;
