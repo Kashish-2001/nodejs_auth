ac983ffc01475cc588eb7ea8d04a2df06b36c3be
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2021-10-12 04:07:59 +0530

    added a post api to test protected routes using jwt

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 // Import Routes
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -16,5 +17,6 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
 app.use(express.json());
 // Route Middlewares
 app.use("/api/user", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(3000, () => console.log("Server up and running"));
