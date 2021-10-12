1bdaebeb2b0f722ac4f770556b6abe750a97b87d
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2021-10-12 22:08:16 +0530

    update port

--- index.js
+++ index.js
@@ -22,4 +22,6 @@ app.use(cors());
 app.use("/api/user", authRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(3000, () => console.log("Server up and running"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("Server up and running"));
